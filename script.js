3e9bc9e1b7f921ec1b389d46bf12623548f27656
cwu90 <canny.wu90@gmail.com>
2022-11-24 22:29:26 -0500

    commit#2

--- script.js
+++ script.js
@@ -73,21 +73,21 @@ let data = [
     heading: 'Bookmark in one click',
     summary:
       'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
-    imgPath: './images/illustration-features-tab-1.svg',
+    imgPath: 'images/illustration-features-tab-1.svg',
     altText: 'dashboard',
   },
   {
     heading: 'Intelligent search',
     summary:
       'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
-    imgPath: './images/illustration-features-tab-2.svg',
+    imgPath: 'images/illustration-features-tab-2.svg',
     altText: 'dashboard with magnifying glass',
   },
   {
     heading: 'Share your bookmarks',
     summary:
       'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
-    imgPath: './images/illustration-features-tab-3.svg',
+    imgPath: 'images/illustration-features-tab-3.svg',
     altText: 'people waving',
   },
 ];
